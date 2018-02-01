31cdcb50a146e36d89385212aef3d12cb140dfd2
mcv16_alumne <mcv16_alumne@A209W10PC01>
2018-02-01 16:55:30 +0100

    Agregamos ia de torreta

--- source/components/comp_ia.cpp
+++ source/components/comp_ia.cpp
@@ -1,16 +1,91 @@
 #include "mcv_platform.h"
 #include "comp_ia.h"
 #include "modules/module_ia.h"
+#include "entity/entity_parser.h"
+#include "comp_circular_controller.h"
+#include "comp_transform.h"
 
 DECL_OBJ_MANAGER("ia", TCompIA);
 
 void TCompIA::debugInMenu() {
+	ImGui::Text("State %s", state.c_str());
+	if (h_target.isValid()) {
+		if (ImGui::TreeNode("Target")) {
+			h_target.debugInMenu();
+			ImGui::TreePop();
+		}
+	}
 }
 
 void TCompIA::update(float dt) {
-  //ai_controller->recalc(dt);
+	if (!h_target.isValid())
+		return;
+
+	(this->*statemap[state])();
+
 }
 
 void TCompIA::load(const json& j, TEntityParseContext& ctx) {
-  ai_controller = CEngine::get().getIA().getNewAIControler(j, CHandle(this) );
+	radius = j.value("radius", 1.0f);
+	speed = j.value("speed", 1.0f);
+	target_name = j.value("target", "");
+	h_target = ctx.findEntityByName(target_name);
+
+	// insert all states in the map
+	AddState("idle", (statehandler)&TCompIA::IdleState);
+	AddState("follow", (statehandler)&TCompIA::FollowState);
+
+	// reset the state
+	ChangeState("idle");
+}
+
+void TCompIA::ChangeState(std::string newstate)
+{
+	// try to find a state with the suitable name
+	if (statemap.find(newstate) == statemap.end())
+	{
+		// the state we wish to jump to does not exist. we abort
+		exit(-1);
+	}
+	state = newstate;
+}
+
+
+void TCompIA::AddState(std::string name, statehandler sh)
+{
+	// try to find a state with the suitable name
+	if (statemap.find(name) != statemap.end())
+	{
+		// the state we wish to jump to does exist. we abort
+		exit(-1);
+	}
+	statemap[name] = sh;
+}
+
+void TCompIA::IdleState()
+{
+	if (GetAsyncKeyState('Q') != 0) ChangeState("follow");
+}
+
+
+void TCompIA::FollowState()
+{
+	if (GetAsyncKeyState('E') != 0) ChangeState("idle");
+
+	curr_yaw += speed * 5;
+
+	CEntity* e_target = h_target;
+	assert(e_target);
+
+	TCompTransform *c_target = e_target->get<TCompTransform>();
+	assert(c_target);
+
+	TCompTransform *c_my_transform = get<TCompTransform>();
+
+	float my_y = c_my_transform->getPosition().y;
+	//VEC3 my_new_pos = c_target->getPosition() + getVectorFromYaw(curr_yaw) * radius;
+	//my_new_pos.y = my_y;
+
+	assert(c_my_transform);
+	c_my_transform->lookAt(c_my_transform->getPosition(), c_target->getPosition());
 }
--- source/components/comp_ia.h
+++ source/components/comp_ia.h
@@ -1,10 +1,30 @@
 #pragma once
 
 #include "comp_base.h"
+#include "geometry/transform.h"
+#include <assert.h>	
+#include <string>
+#include <map>
 
-class IAIController;
+using namespace std;
+
+class TCompIA;
  
+typedef void (TCompIA::*statehandler)();
+
 class TCompIA : public TCompBase {
+	CHandle     h_target;
+	std::string target_name;
+	float       speed = 1.0f;
+	float       curr_yaw = 0.f;
+	float       radius = 1.0f;
+
+	string state;
+	// the states, as maps to functions
+	map<string, statehandler>statemap;
+
+	DECL_SIBLING_ACCESS();
+
 public:
 
   IAIController* ai_controller = nullptr;
@@ -12,4 +32,11 @@ public:
   void debugInMenu();
   void update(float dt);
   void load(const json& j, TEntityParseContext& ctx);
+
+  // Dani
+  void ChangeState(string);	// state we wish to go to
+  void AddState(string, statehandler);
+
+  void IdleState();
+  void FollowState();
 };
