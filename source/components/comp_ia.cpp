43a38958fe0b03e85dc3cf7bf46222f0796548b5
mcv16_alumne <mcv16_alumne@IDEC-UPF.LOCAL>
2018-02-01 18:22:33 +0100

    Sustituyo por codigo de edu con chase incluido

--- source/components/comp_ia.cpp
+++ source/components/comp_ia.cpp
@@ -1,43 +1,57 @@
 #include "mcv_platform.h"
 #include "comp_ia.h"
 #include "modules/module_ia.h"
+#include "mcv_platform.h"
 #include "entity/entity_parser.h"
-#include "comp_circular_controller.h"
 #include "comp_transform.h"
+#include "geometry\SimpleMath.h"
+#include <iostream>
 
 DECL_OBJ_MANAGER("ia", TCompIA);
 
 void TCompIA::debugInMenu() {
 	ImGui::Text("State %s", state.c_str());
+	ImGui::Text("Distance %f", distance);
 	if (h_target.isValid()) {
 		if (ImGui::TreeNode("Target")) {
 			h_target.debugInMenu();
 			ImGui::TreePop();
+
 		}
 	}
 }
 
+void TCompIA::load(const json& j, TEntityParseContext& ctx) {
+	target_name = j.value("target", "");
+	alcance = j.value("alcance", 1.0f);
+	chase_distance = j.value("chase_distance", 1.0f);
+
+	h_target = ctx.findEntityByName(target_name);
+	AddState("idle", (statehandler)&TCompIA::IdleState);
+	AddState("follow", (statehandler)&TCompIA::FollowState);
+	AddState("chase", (statehandler)&TCompIA::ChaseState);
+	ChangeState("idle");
+}
+
 void TCompIA::update(float dt) {
+
 	if (!h_target.isValid())
 		return;
 
+	CEntity* e_target = h_target;
+	assert(e_target);
+
+	TCompTransform *c_target = e_target->get<TCompTransform>();
+	assert(c_target);
+
+	TCompTransform *c_my_transform = get<TCompTransform>();
+
+	distance = Vector3::Distance(c_my_transform->getPosition(), c_target->getPosition());
+
 	(this->*statemap[state])();
-
 }
 
-void TCompIA::load(const json& j, TEntityParseContext& ctx) {
-	radius = j.value("radius", 1.0f);
-	speed = j.value("speed", 1.0f);
-	target_name = j.value("target", "");
-	h_target = ctx.findEntityByName(target_name);
 
-	// insert all states in the map
-	AddState("idle", (statehandler)&TCompIA::IdleState);
-	AddState("follow", (statehandler)&TCompIA::FollowState);
-
-	// reset the state
-	ChangeState("idle");
-}
 
 void TCompIA::ChangeState(std::string newstate)
 {
@@ -64,15 +78,17 @@ void TCompIA::AddState(std::string name, statehandler sh)
 
 void TCompIA::IdleState()
 {
-	if (GetAsyncKeyState('Q') != 0) ChangeState("follow");
+	if (distance < alcance) ChangeState("follow");
 }
 
 
 void TCompIA::FollowState()
 {
-	if (GetAsyncKeyState('E') != 0) ChangeState("idle");
+	if (distance >= alcance) ChangeState("idle");
+	else if (distance < chase_distance) ChangeState("chase");
 
-	curr_yaw += speed * 5;
+	if (!h_target.isValid())
+		return;
 
 	CEntity* e_target = h_target;
 	assert(e_target);
@@ -82,10 +98,35 @@ void TCompIA::FollowState()
 
 	TCompTransform *c_my_transform = get<TCompTransform>();
 
-	float my_y = c_my_transform->getPosition().y;
-	//VEC3 my_new_pos = c_target->getPosition() + getVectorFromYaw(curr_yaw) * radius;
-	//my_new_pos.y = my_y;
+	assert(c_my_transform);
+
+	c_my_transform->lookAt(c_my_transform->getPosition(), c_target->getPosition());
+}
+
+
+
+void TCompIA::ChaseState()
+{
+	if (distance >= chase_distance + 2.f) ChangeState("follow");
+
+	if (!h_target.isValid())
+		return;
+
+	CEntity* e_target = h_target;
+	assert(e_target);
+
+	TCompTransform *c_target = e_target->get<TCompTransform>();
+	assert(c_target);
+
+	TCompTransform *c_my_transform = get<TCompTransform>();
 
 	assert(c_my_transform);
-	c_my_transform->lookAt(c_my_transform->getPosition(), c_target->getPosition());
+
+	VEC3 step = c_target->getPosition() - c_my_transform->getPosition();
+	step.Normalize();
+	VEC3 my_new_pos = c_my_transform->getPosition() + step *0.001f;
+
+
+	c_my_transform->lookAt(my_new_pos, c_target->getPosition());
+
 }
--- source/components/comp_ia.h
+++ source/components/comp_ia.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "comp_base.h"
-#include "geometry/transform.h"
 #include <assert.h>	
 #include <string>
 #include <map>
@@ -9,34 +8,32 @@
 using namespace std;
 
 class TCompIA;
- 
 typedef void (TCompIA::*statehandler)();
 
+
 class TCompIA : public TCompBase {
+public:
+	TCompIA* ai_controller = nullptr;
 	CHandle     h_target;
 	std::string target_name;
-	float       speed = 1.0f;
-	float       curr_yaw = 0.f;
-	float       radius = 1.0f;
+	float       distance;
+	float				alcance;
+	float				chase_distance;
 
 	string state;
 	// the states, as maps to functions
-	map<string, statehandler>statemap;
+	map<string, statehandler> statemap;
 
 	DECL_SIBLING_ACCESS();
 
 public:
+	void debugInMenu();
+	void load(const json& j, TEntityParseContext& ctx);
+	void update(float dt);
+	void ChangeState(string);	// state we wish to go to
+	void AddState(string, statehandler);
 
-  IAIController* ai_controller = nullptr;
-
-  void debugInMenu();
-  void update(float dt);
-  void load(const json& j, TEntityParseContext& ctx);
-
-  // Dani
-  void ChangeState(string);	// state we wish to go to
-  void AddState(string, statehandler);
-
-  void IdleState();
-  void FollowState();
+	void IdleState();
+	void FollowState();
+	void ChaseState();
 };
